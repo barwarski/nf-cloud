79bcb8223ea29539dd52f2f9e1292881dfded897
barwarski <awien.bar@gmail.com>
2022-12-22 16:01:55 +0100

    add node fs:empty

--- nf_cloud_frontend/nuxt.config.js
+++ nf_cloud_frontend/nuxt.config.js
@@ -48,6 +48,14 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    extend (config, { isDev, isClient }) {
+
+      config.node= {
+         fs: 'empty'
+       }
+ 
+      // ....
+   }
   },
 
   publicRuntimeConfig: {
